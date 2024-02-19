16b5b2a7b813be77ace85a901e3b97eac918e1bd
Davidson <dehpacifico@gmail.com>
2024-02-18 21:33:40 -0300

    criar controllers

--- src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package me.dio.service.impl;
 import me.dio.domain.model.User;
 import me.dio.domain.repository.UserRepository;
 import me.dio.service.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
